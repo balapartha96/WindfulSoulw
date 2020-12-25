e577624e0d4da50f29ef06c36f09eba8687e3eb7
balapartha96 <kmnr24366ssc@sina.com>
2020-12-26 00:05:52 +0800

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>WindfulSoul Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '	#00BFFF',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
